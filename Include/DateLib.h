d9025bce54ded4af521e7ca78729cce2d58c5af4
HausdorffQ <30655679+HausdorffQ@users.noreply.github.com>
2018-03-09 14:07:33 +0800

    update

---
+++ Include/DateLib.h
@@ -0,0 +1,83 @@
+/******************************************************************************
+ * 作者：朱清豪                                                                
+ * 时间：2018.3.9                                                             
+ * 描述：定义了一个以链表结构实现的顺序表，声明了与顺序表相关的操作             
+ *****************************************************************************/
+#ifndef DATELIB_H
+#define DATELIB_H
+#include "Music.h"
+#include "Queue.h"
+#include <stdio.h>
+
+/*
+ *声明一个全局变量，存储歌曲库的路径
+ */
+extern String lib_addr;
+
+/*
+ *将歌曲库的路径初始化
+ */
+extern Status lib_addr_init();
+
+/*
+ *将歌曲库的路径更改为new_addr
+ */
+extern Status lib_addr_change(String *new_addr);
+
+/*
+ *初始化歌曲库
+ */
+extern Status lib_init();
+
+/*
+ *往歌曲库中增加一首歌曲music
+ */
+extern Status lib_add(Music *music);
+
+/*
+ *将music这首歌从歌曲库中删除
+ */
+extern Status lib_delete(Music *music);
+
+/*
+ *将整个歌曲库排序
+ */
+extern Status lib_sort();
+
+/*
+ *按照关键字key_word搜索歌曲库，将结果放入队列result中
+ */
+extern Status lib_serch(String *key_word,
+                        Queue *result);
+
+ /*
+ *按照歌曲名name搜索歌曲库，将结果放入队列result中
+ */                       
+extern Status lib_serch_name(String *name,
+                             Queue *result);
+
+/*
+ *按照歌手singer搜索歌曲库，将结果放入队列result中
+ */
+extern Status lib_serch_singer(String *singer,
+                               Queue *result);
+
+/*
+ *按照词作者songwriter搜索歌曲库，将结果放入队列result中
+ */
+extern Status lib_serch_songwriter(String *songwriter,
+                                   Queue *result);
+
+/*
+ *按照曲作者composer搜索歌曲库，将结果放入队列result中
+ */
+extern Status lib_serch_composer(String *composer,
+                                 Queue *result);
+
+/*
+ *按照歌曲风格搜索歌曲库，将结果放入队列result中
+ */
+extern Status lib_serch_style(String *style,
+                              Queue *result);
+
+#endif
--- Include/Music.h
+++ Include/Music.h
@@ -1,12 +1,14 @@
 /******************************************************************************
  * 作者：朱清豪                                                                
  * 时间：2018.3.6                                                             
- * 描述：定义所需使用的基本数据类型               
+ * 描述：定义所需使用的基本数据类型，声明基本的数据转换函数               
  *****************************************************************************/
 #ifndef MUSIC_H
 #define MUSIC_H
 #include"String.h"
-
+#ifndef LIBDATE
+#define LIBDATE char *
+#endif
 typedef struct Music
 {
     double duration;            //歌曲时长
@@ -17,6 +19,19 @@ typedef struct Music
     struct String style;        //风格
     struct String address;      //歌曲存放路径
 }Music,Music;
+
+/*
+ *将Music数据类型转换成用于数据存储的LIBDATE数据类型
+ *2018.3.9增添内容by朱清豪
+ */
+Status music_to_lib(Music *music, LIBDATE ld);
+
+/*
+ *将LIBDATE数据类型转换成Music数据类型
+ *2018.3.9增添内容by朱清豪
+ */
+Status lib_to_music(LIBDATE ld, Music *music);
+
 
 
 #endif
